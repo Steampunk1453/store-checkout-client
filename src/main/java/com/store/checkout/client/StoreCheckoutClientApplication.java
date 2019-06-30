0e37e5585e3654373f564a21aa6dcef19cd09cfd
Steampunk1453 <ddda>
2019-06-30 17:27:50 +0200

    Initial commit

---
+++ src/main/java/com/store/checkout/client/StoreCheckoutClientApplication.java
@@ -0,0 +1,13 @@
+package com.store.checkout.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCheckoutClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCheckoutClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/checkout/client/StoreCheckoutClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.store.checkout.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreCheckoutClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
