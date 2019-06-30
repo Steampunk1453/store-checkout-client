0fdd7f59bcae984d64954948f4c2a1a71fb32175
Steampunk1453 <ddda>
2019-06-30 23:13:46 +0200

    change format

--- src/main/java/com/store/checkout/client/dtos/BasketResponse.java
+++ src/main/java/com/store/checkout/client/dtos/BasketResponse.java
@@ -1,10 +1,8 @@
 package com.store.checkout.client.dtos;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -12,8 +10,7 @@ import java.util.List;
 public class BasketResponse {
 
     private long id;
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    private LocalDate dateCreated;
+    private String dateCreated;
     private String status;
     private List<BasketProduct> basketProducts;
     private double totalAmount;
